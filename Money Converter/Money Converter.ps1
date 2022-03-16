44600d310affbc8bf03a1144fb3ff1bc3f5ec432
Richard S. Beredo <1176079092121005@mil>
2022-03-16 16:52:04 -0400

    Update 1

---
+++ Money Converter/Money Converter.ps1
@@ -0,0 +1,81 @@
+﻿#Money Converter
+#Script by: Rich Beredo
+#Reference: https://www.altaro.com/msp-dojo/web-scraping-tool-for-msps/
+#https://sid-500.com/2017/12/19/powershell-playing-with-text-or-how-to-get-every-x-line-from-text-output-trimreplacesplit/
+#youtube: ep1 how to create a WPF GUI for your Powershell Script
+#Last Update 09MAR2022
+
+clear-host
+write-host "------NEW SESSION-------"
+Add-Type -AssemblyName presentationframework, presentationcore
+
+#Website
+$website = "https://www.x-rates.com/table/?from=USD&amount=1"
+$webRequest = Invoke-WebRequest $website
+
+
+#Parse Data
+$rawData = $webRequest.ParsedHtml.all.tags("td") | ForEach-Object -MemberName innertext
+$parsedData = $rawData | Select-Object -Skip 30
+
+
+#Values
+$currency = For ($i=0; $i -lt $parsedData.count; $i+=3){$parsedData[$i]}
+$rate = For ($i=1; $i -lt $parsedData.count; $i+=3){$parsedData[$i]}
+
+
+#XAML File
+$xamlLocation = "Z:\Powershell\Rich-Scripts\Money Converter\ConvertGUI\ConvertGUI\MainWindow.xaml"
+$xamlRaw = Get-Content -Path $xamlLocation -Raw
+
+#Cleans up XAML
+$xamlRaw = $xamlRaw -replace 'mc:Ignorable="d"', '' -replace "x:N", 'N' -replace '^<Win.*', '<Window'
+[xml]$xamlFile = $xamlRaw
+
+#Read XAML
+$reader = (New-Object System.Xml.XmlNodeReader $xamlFile)
+
+#Load GUI
+$gui = [Windows.Markup.XamlReader]::Load( $reader )
+
+
+
+$wpf = @{ }
+$xamlFile.SelectNodes("//*[@Name]") | ForEach-Object {
+    $wpf.Add($_.Name, $gui.FindName($_.Name))
+   
+}
+$wpf.baseCurrencyList.Items.Add("US Dollar")
+
+foreach ($currency in $currency) {
+    $wpf.newCurrencyList.Items.Add($currency)
+}
+
+
+$wpf.execute.Add_Click({
+    $arrayPlace = $wpf.newCurrencyList.SelectedIndex
+    $chosenRate = $rate[$arrayPlace]
+    $prevEntered = $wpf.prevAmmount.Text
+    
+    [Decimal]$prevEntered = $prevEntered
+    [Decimal]$calcAmmount = $calcAmmount
+    $calcAmmount = $prevEntered * $chosenRate
+    $wpf.newAmmount.Text = $calcAmmount
+})
+
+
+
+$wpf.Exit.Add_Click({$gui.Close()})
+
+
+
+$null = $gui.ShowDialog()
+
+
+
+
+
+
+
+
+
